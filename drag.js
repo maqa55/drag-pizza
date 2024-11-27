ffcb7e0cccde69b0455a844079bb448da32540aa
maqa55 <maqamet8@gmail.com>
2024-11-27 09:17:26 +0400

    p

--- drag.js
+++ drag.js
@@ -2,10 +2,11 @@
 
 const t = document.querySelectorAll(".ref .terkib");
 const pizza = document.querySelector(".m .pizza");
+const pomidor1 = document.querySelector(".ref .pomidor1")
 
 for (let i = 0; i < t.length; i++) {
     t[i].addEventListener("dragstart", function (a) {
-        a.dataTransfer.setData("m", this.id)
+        a.dataTransfer.setData("d", this.id)
     })
 }
 
@@ -17,9 +18,14 @@ pizza.addEventListener("dragover", function (e) {
 
 // })
 
+pizza.addEventListener("mousemove", function () {
+    setTimeout(() => {
+        pomidor1.classList.add("p")
+    }, 1000);
+})
 
 pizza.addEventListener("drop", function (s) {
-    let id = s.dataTransfer.getData("m")
+    let id = s.dataTransfer.getData("d")
     let terkib = document.getElementById(id)
     pizza.append(terkib)
 })
